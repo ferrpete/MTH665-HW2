5842ca6c7fb5e035003467bdea7d0160e1fbc688
ferrpete <32526300+ferrpete@users.noreply.github.com>
2018-02-14 15:04:58 -0800

    Changed plots
    Changed plots

--- Main.m
+++ Main.m
@@ -10,12 +10,6 @@ N = length(n);
 x = [0:0.001:1]';
 ExactSol = Exact(x);
 
-figure(1)
-plot(x, ExactSol, 'k')
-xlabel('x')
-ylabel('u(x)')
-hold on
-
 for i = 1:N
     
     [FemSol, x] = SimpleFEM1D(n(i));
@@ -32,7 +26,6 @@ for i = 1:N
     end
     
     errorE(i) = sqrt(Simpson13Approx(n(i),x(2:end),dError.^2));
-    plot(x,FemSol,'-o')
     
 end
 
@@ -40,26 +33,9 @@ a = 0; % left endpoint
 b = 1; % right endpoint
 h = (b-a)./n; % uniform mesh size
 
-legend('Exact', 'h = 0.5', 'h = 0.25', 'h = 0.125')
-hold off
-
-figure(2)
+figure(1)
 loglog(h,h,'k--',h,h.^2,'k-',h,errorMax,'*-r',h,errorL2,'*-b',h,errorE,'*-c')
 xlabel('Mesh size, h', 'Interpreter', 'latex')
 ylabel('Error, e(h)', 'Interpreter', 'latex')
 legend({'Linear', 'Quadratic', '$\max_i |u(x_i)-u_k(x_i)|$', '$\left \| u - u_k \right \|_{L^2}$', '$\left \| u - u_k \right \|_E$'}, 'Interpreter', 'latex')
-legend('Location', 'southeast')
-
-figure(3)
-loglog(h,errorE,'*-c',h,h.*fL2,'k-')
-title('Convergence of FEM Solution in Energy Norm', 'Interpreter', 'latex')
-xlabel('Mesh size, h','Interpreter','latex')
-legend({'$\left \| u - u_k \right \|_E$', '$h \left \| f \right \|_{L^2}$'}, 'Interpreter', 'latex')
-legend('Location', 'southeast')
-
-figure(4)
-loglog(h,errorL2,'*-b',h,(h.^2).*fL2,'k-')
-title('Convergence of FEM Solution in $L_2$ Norm', 'Interpreter', 'latex')
-xlabel('Mesh size, h','Interpreter','latex')
-legend({'$\left \| u - u_k \right \|_{L^2}$', '$h^2 \left \| f \right \|_{L^2}$'}, 'Interpreter', 'latex')
 legend('Location', 'southeast')
